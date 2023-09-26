f99c5b0bad05fe665d26452b4138704e31b0c390
Stepan Sotnikov <sotnikovse1207@gmail.com>
2023-09-26 19:21:21 +0900

    pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import Inspect from 'vite-plugin-inspect'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   appType: 'mpa',
@@ -11,5 +12,46 @@ export default defineConfig({
       },
     },
   },
-  plugins: [Inspect()],
+  plugins: [
+    Inspect(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,wasm}'],
+      },
+      manifest: {
+        lang: 'ru',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
+  // server: {
+  //   https: {
+  //     key: './local-certificates/localhost.key',
+  //     cert: './local-certificates/localhost.crt',
+  //   },
+  // },
 })
